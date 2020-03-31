228ee9f88c2b89a78d16b908f6fe95f32a1ba072
winzurk <zwinzurk@asu.edu>
2020-03-31 12:37:02 -0700

    Updates to __init__.py

--- torchtraps/__init__.py
+++ torchtraps/__init__.py
@@ -4,6 +4,6 @@ __author__ = """Zac Winzurk"""
 __email__ = 'zwinzurk@asu.edu'
 __version__ = '0.1.0'
 
-from .coco_camera_traps_loader import *
-from .imagenet import imagenet_classes
-from .lightning import *
+# from .coco_camera_traps_loader import *
+# from .imagenet import imagenet_classes
+# from .lightning import *
